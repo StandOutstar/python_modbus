d5cb5c8d73bd2236012d9bae1c7404fd66b8b19b
StandOutstar <729901070@qq.com>
2016-10-13 16:37:44 +0800

    添加py2exe的打包文件setup.py
    在命令行里执行setup.py

---
+++ test_modbus/setup.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+import py2exe
+import sys
+
+# this allows to run it with a simple double click.
+sys.argv.append('py2exe')
+
+py2exe_options = {
+        "includes": ["sip"],  # 如果打包文件中有PyQt代码，则这句为必须添加的
+        # "dll_excludes": ["msvcr100.dll"],  # 这句必须有，不然打包后的程序运行时会报找不到MSVCP90.dll，如果打包过程中找不到这个文件，请安装相应的库
+        "compressed": 1,
+        "optimize": 2,
+        "ascii": 0,
+        "bundle_files": 1,  # 关于这个参数请看第三部分中的问题(2)
+        }
+
+setup(
+      name = 'PyQt Demo',
+      version = '1.0',
+      windows = ['test_pyqt.py'],   # 括号中更改为你要打包的代码文件名
+      data_files=[("",
+                   [r"C:\Python34\Lib\site-packages\PyQt5\libEGL.dll"]),
+                  ("platforms",
+                   [r"C:\Python34\Lib\site-packages\PyQt5\plugins\platforms\qwindows.dll"])],
+      zipfile = None,
+      options = {'py2exe': py2exe_options}
+      )
